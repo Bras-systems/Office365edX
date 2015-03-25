83b24a87ac2cc4832b4989e3abfe971a4459b67e
GeekTrainer <chrhar@microsoft.com>
2015-03-25 14:16:06 -0700

    Module 3 recordings

---
+++ Office Apps Demos/OutlookAppDemoWeb/AppRead/Home/Home.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The Office initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            displayItemDetails();
+        });
+    };
+
+    // Displays the "Subject" and "From" fields, based on the current mail item
+    function displayItemDetails() {
+        var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
+        var meetingSuggestions = item.getEntitiesByType(Office.MailboxEnums.EntityType.MeetingSuggestion);
+        var meetingSuggestion = meetingSuggestions[0];
+
+        $('#start-time').text(meetingSuggestion.start);
+    }
+})();
